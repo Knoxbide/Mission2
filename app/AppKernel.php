5d0f725e11c843e5186a4230296daad983cd0beb
Jay <patel.jay@live.fr>
2014-10-10 10:53:21 +0000

    inital commit jay

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,7 +16,7 @@ class AppKernel extends Kernel
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
-            new Acme\MissionBundle\AcmeMissionBundle(),
+            new Acme\MissionBundle\MissionBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
